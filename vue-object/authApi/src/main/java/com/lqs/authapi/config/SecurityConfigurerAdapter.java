92d920ec9a7f3ffcbdcecc5a33389164861e8f7c
lqs2002 <749062870@qq.com>
2022-06-17 15:18:35 +0800

    finish

--- vue-object/authApi/src/main/java/com/lqs/authapi/config/SecurityConfigurerAdapter.java
+++ vue-object/authApi/src/main/java/com/lqs/authapi/config/SecurityConfigurerAdapter.java
@@ -7,10 +7,12 @@ import com.lqs.authapi.handler.MyAccessDeniedHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsUtils;
 
 @Configuration
+//@EnableWebSecurity
 public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
 
